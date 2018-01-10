0b37b167a2d740735f64592c11c7c07bab581c09
zhouyingkai1 <zhou2zhu>
2018-01-10 11:48:08 +0800

    提交

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 react-native + dva + react-navigation
+
+## Screenshot
+
+![home](https://github.com/zhouyingkai1/react-native-dva/tree/master/pic/home.png)
+![drawer](https://github.com/zhouyingkai1/react-native-dva/tree/master/pic/drawer.png)
--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity, TextInput, StatusBar } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import { connect } from 'react-redux'
 import pxToDp from '../utils/pxToDp'
 import { Header  } from '../components'
 import Icon from 'react-native-vector-icons/Ionicons';
-import { NavigationActions } from '../utils'
 
 class Home extends Component {
   static navigationOptions = {
@@ -21,22 +20,16 @@ class Home extends Component {
     this.props.navigation.navigate('Login')
   }
   render() {
-    const { coursewareList } = this.props.app
     return (
       <View>
-        <Header title='图书详情' {...this.props}/>
+        <Header {...this.props}/>
         <Text onPress={this.handlePress}>{this.props.router.routes.length<= 1 && '最外'}</Text>
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
-  icon: {
-    width: pxToDp(46),
-    height: pxToDp(46),
-  },
-})
+
 const mapStateToProps = ({app, router})=> {
   return {app, router}
 }
